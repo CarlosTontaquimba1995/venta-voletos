825e4b998703b59b8b26808a9d904e9d51b788da
Carlos Vicente Tontaquimba Quinchuqui <Carlos.Tontaquimba@fj.local>
2026-01-16 11:26:15 -0500

    variables de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://qr-backend-bloy.onrender.com',
+  apiUrlPawkar: 'https://pawkar-backend.onrender.com',
   version: '0.0.1',
   environment: 'production'
 };
